8bb79c170b05022d03cacfc9b8497453c85d4173
Julia Hunter <Julia.Hunter@ijyi.com>
2020-08-20 13:23:49 +0100

    undo text 001

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace SimpleWebApp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World 001!");
                 });
             });
         }
