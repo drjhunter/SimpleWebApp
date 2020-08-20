74dae0aa89993ef23f64c13a6d8a9cce3af1da09
Julia Hunter <Julia.Hunter@ijyi.com>
2020-08-20 13:55:09 +0100

    Add 005

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace SimpleWebApp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World 004!");
+                    await context.Response.WriteAsync("Hello World 005!");
                 });
             });
         }
